4fd0a2794caa3d6fe8565561d0057426b83a1da1
Stephen Wright <stephen.c.wright@gmail.com>
2018-03-14 14:10:24 -0400

    initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        //"react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Sudoku Single Page Application
+
+A single page application in react to interact with sudoku service
